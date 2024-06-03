53029bddb205a47efc393a018e6702375d8844fd
somogyidavid94@gmail.com <somogyidavid94@gmail.com>
2024-06-03 15:29:37 +0200

    test commit

--- app.py
+++ app.py
@@ -4,15 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return hello
-
-
-@app.route('/index')
 def index():
     return render_template('index.html')
-
-
 
 
 if __name__ == '__main__':
